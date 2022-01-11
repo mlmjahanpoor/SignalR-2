3e24d8f60752a0700d06d48bd68fceea9518b8eb
Moslem jahanpoor <mlmjahanpoor@gmail.com>
2022-01-11 15:09:45 +0330

    init

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+
+            var id = Context.ConnectionId;
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task ShowMessage(string name)
+        {
+            await Clients.All.SendAsync("ShowMessage", name);
+        }
+    }
+}
